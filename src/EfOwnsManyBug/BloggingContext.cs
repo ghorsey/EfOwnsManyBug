ef6e8527f286cde3f410857ea80e0227e2dc60a6
ghorsey <geoff.horsey@outlook.com>
2025-03-12 15:26:55 -0700

    feat: added migration and initial entities

---
+++ src/EfOwnsManyBug/BloggingContext.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfOwnsManyBug;
+
+public class BloggingContext : DbContext
+{
+    DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .UseSqlServer($"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory()}\\Data\\SampleDatabase.mdf;Integrated Security=True")
+            .UseSeeding((ctx, _) =>
+            {
+                var postId = new PostId(Guid.Parse("7EE68A49-843E-4274-86B3-B80CFFE8D407"));
+                var post = ctx.Set<Post>().FirstOrDefault(p => p.Id == postId);
+
+                if (post == null)
+                {
+                    ctx.Set<Post>().Add(
+                        new Post
+                        {
+                            Id = postId,
+                            Body = "my test post",
+                            Tags = new List<PostTag>
+                            {
+                                new PostTag
+                                {
+                                    PostId =postId,
+                                    Tag = "#tag-one"
+                                },
+                                new PostTag
+                                {
+                                    PostId =postId,
+                                    Tag = "#tag-two"
+                                },
+                            }
+                        }
+                    );
+                    ctx.SaveChanges();
+                }
+            });
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        var postBuilder = modelBuilder.Entity<Post>();
+
+        postBuilder.HasKey(d => d.Id);
+        postBuilder.Property(d => d.Id)
+            .IsRequired()
+            .HasConversion(v => v.Value, v => new PostId(v))
+            .ValueGeneratedNever();
+
+        postBuilder.Property(d => d.Body)
+            .IsRequired()
+            .HasDefaultValue(string.Empty)
+            .HasMaxLength(1000);
+
+        postBuilder.OwnsMany(
+            d => d.Tags,
+            tags =>
+            {
+                tags.WithOwner()
+                    .HasForeignKey(tag => tag.PostId);
+                tags.Property<Guid>("Id");
+                tags.HasKey("Id");
+
+                tags.Property(tag => tag.Tag)
+                    .IsRequired()
+                    .HasDefaultValue(string.Empty)
+                    .HasMaxLength(200);
+            }
+        );
+            
+    }
+}
+
+public record struct PostId(Guid Value);
+
+public class Post
+{
+    public PostId Id { get; set; } = new PostId(Guid.NewGuid());
+
+    public string Body { get; set; } = string.Empty;
+
+    public List<PostTag> Tags { get; init; } = new();
+}
+
+public class PostTag
+{
+    public PostId PostId { get; set; } = new PostId(Guid.NewGuid());
+    public string Tag { get; set; } = string.Empty;
+}
+
---
+++ src/EfOwnsManyBug/Migrations/20250312214842_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfOwnsManyBug.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false, defaultValue: "")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostTag",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Tag = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false, defaultValue: "")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostTag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostTag_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostTag_PostId",
+                table: "PostTag",
+                column: "PostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostTag");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
