fe72cf59c2f1a9c1aaa2402579068ee8b4341823
ghorsey <geoff.horsey@outlook.com>
2025-03-13 12:25:25 -0700

    chore: tweaked code slightly

--- src/EfOwnsManyBug/Program.cs
+++ src/EfOwnsManyBug/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EfOwnsManyBug;
 using EfOwnsManyBug.Repository;
 using Microsoft.Extensions.Logging;
+using System.Data;
 using System.Text.Json;
 
 var ctx = new BloggingContext();
@@ -9,24 +10,30 @@ var logger = loggerFactory.CreateLogger("Program");
 var uow = new UnitOfWork(ctx, loggerFactory);
 
 var ct = new CancellationToken();
-var post = await uow.PostRepository.FindAsync(BloggingContext.PostId, ct);
+await uow.ExecuteInResilientTransactionAsync(
+    async () =>
+    {
+        var post = await uow.PostRepository.FindAsync(BloggingContext.PostId, ct);
 
-if (post == null)
-{
-    logger.LogError("The post does not exist");
-    return;
-}
+        if (post == null)
+        {
+            logger.LogError("The post does not exist");
+            return false;
+        }
 
-var seralizerSettings = new JsonSerializerOptions
-{
-    IncludeFields = true,
-};
+        var seralizerSettings = new JsonSerializerOptions
+        {
+            IncludeFields = true,
+        };
 
-Console.WriteLine("The found POST with the owned collection:");
-Console.WriteLine(JsonSerializer.Serialize(post, seralizerSettings));
+        Console.WriteLine("The found POST with the owned collection:");
+        Console.WriteLine(JsonSerializer.Serialize(post, seralizerSettings));
 
-Console.WriteLine("");
-Console.WriteLine("Attempting to delete the record");
-await uow.PostRepository.RemoveAsync(post);
-await uow.CommitAsync();
-
+        Console.WriteLine("");
+        Console.WriteLine("Attempting to delete the record");
+        await uow.PostRepository.RemoveAsync(post);
+        await uow.CommitAsync();
+        return true;
+    },
+    IsolationLevel.ReadUncommitted,
+    ct);
