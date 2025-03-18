2219fd0a70a5f136cd7c1e20f40ab72a7ef1997e
ghorsey <geoff.horsey@outlook.com>
2025-03-17 18:58:43 -0700

    feat: removed my uow/repo libraries

--- src/EfOwnsManyBug/Program.cs
+++ src/EfOwnsManyBug/Program.cs
@@ -1,40 +1,26 @@
 ﻿using EfOwnsManyBug;
-using EfOwnsManyBug.Repository;
-using Microsoft.Extensions.Logging;
-using System.Data;
+using EfOwnsManyBug.Models;
 using System.Text.Json;
 
-var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
 var ctx = new BloggingContext();
-var logger = loggerFactory.CreateLogger("Program");
-var uow = new UnitOfWork(ctx, loggerFactory);
 
-var ct = new CancellationToken();
+var post = ctx.Set<PostSummary>().Find(BloggingContext.PostId);
 
-await uow.ExecuteInResilientTransactionAsync(
-    async () =>
-    {
-        var post = await uow.PostSummaryRepository.FindAsync(BloggingContext.PostId, ct);
+if (post == null)
+{
+    Console.WriteLine("The post does not exist, run the command 'dotnet ef -p .\\src\\EfOwnsManyBug database update' to seed the database");
+    return;
+}
 
-        if (post == null)
-        {
-            logger.LogError("The post does not exist");
-            return false;
-        }
+JsonSerializerOptions seralizerSettings = new()
+{
+    WriteIndented = true
+};
 
-        JsonSerializerOptions seralizerSettings = new()
-        {
-            WriteIndented = true
-        };
+Console.WriteLine("The found POST with the owned collection:");
+Console.WriteLine(JsonSerializer.Serialize(post, seralizerSettings));
 
-        Console.WriteLine("The found POST with the owned collection:");
-        Console.WriteLine(JsonSerializer.Serialize(post, seralizerSettings));
-
-        Console.WriteLine("");
-        Console.WriteLine("Attempting to delete the record");
-        await uow.PostSummaryRepository.RemoveAsync(post);
-        await uow.CommitAsync();
-        return true;
-    },
-    IsolationLevel.ReadUncommitted,
-    ct);
+Console.WriteLine("");
+Console.WriteLine("Attempting to delete the record");
+ctx.Set<PostSummary>().Remove(post);
+ctx.SaveChanges();
